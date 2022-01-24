3927000d391ba9dbf8a7b027e4208f9b7c69b063
agarcia101 <98313753+agarcia101@users.noreply.github.com>
2022-01-24 10:31:06 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,604 @@
+function check() {
+	var di = 0;
+	var df = 0;
+	var at = 0;
+	var an = 0;
+	var ps = 0;
+
+	var q1 = document.getElementsByName('q1');
+
+for (var i = 0, length = q1.length; i < length; i++)
+{
+ if (q1[i].checked)
+ {
+
+ 	if (q1[i].value == "di0") {
+ 		di = di+0;  
+ 	else if (q1[i].value == "di1") {
+		di = di+1;  
+	else if (q1[i].value == "di2") {
+		di = di+2;
+	else if (q1[i].value == "di3") {
+			di = di+3;
+  break;
+ }
+}
+
+var q2 = document.getElementsByName('q2');
+
+for (var i = 0, length = q2.length; i < length; i++)
+{
+ if (q2[i].checked)
+ {
+
+	if (q2[i].value == "di0") {
+		di = di+0;  
+	else if (q2[i].value == "di1") {
+	   di = di+1;  
+   else if (q2[i].value == "di2") {
+	   di = di+2;
+   else if (q2[i].value == "di3") {
+		   di = di+3;
+
+  break;
+ }
+}
+
+var q3 = document.getElementsByName('q3');
+
+for (var i = 0, length = q3.length; i < length; i++)
+{
+ if (q3[i].checked)
+ {
+
+	if (q3[i].value == "di0") {
+		di = di+0;  
+	else if (q3[i].value == "di1") {
+	   di = di+1;  
+   else if (q3[i].value == "di2") {
+	   di = di+2;
+   else if (q3[i].value == "di3") {
+		   di = di+3;
+
+  break;
+ }
+}
+
+var q4 = document.getElementsByName('q4');
+
+for (var i = 0, length = q4.length; i < length; i++)
+{
+ if (q4[i].checked)
+ {
+
+	if (q4[i].value == "df0") {
+		df = df+0;  
+	else if (q4[i].value == "df1") {
+	   df = df+1;  
+   else if (q4[i].value == "df2") {
+	   df = df+2;
+   else if (q4[i].value == "df3") {
+		   df = df+3;
+
+  break;
+ }
+}
+
+var q5 = document.getElementsByName('q5');
+
+for (var i = 0, length = q5.length; i < length; i++)
+{
+ if (q5[i].checked)
+ {
+
+	if (q5[i].value == "di0") {
+		di = di+0;  
+	else if (q5[i].value == "di1") {
+	   di = di+1;  
+   else if (q5[i].value == "di2") {
+	   di = di+2;
+   else if (q5[i].value == "di3") {
+		   di = di+3;
+
+  break;
+ }
+}
+
+var q6 = document.getElementsByName('q6');
+
+for (var i = 0, length = q6.length; i < length; i++)
+{
+ if (q5[i].checked)
+ {
+
+	if (q6[i].value == "di0") {
+		di = di+0;  
+	else if (q6[i].value == "di1") {
+	   di = di+1;  
+   else if (q6[i].value == "di2") {
+	   di = di+2;
+   else if (q6[i].value == "di3") {
+		   di = di+3;
+
+  break;
+ }
+}
+
+var q7 = document.getElementsByName('q7');
+
+for (var i = 0, length = q7.length; i < length; i++)
+{
+ if (q7[i].checked)
+ {
+
+	if (q7[i].value == "ps0") {
+		ps = ps+0;  
+	else if (q7[i].value == "ps1") {
+	   ps = ps+1;  
+   else if (q7[i].value == "ps2") {
+	   ps = ps+2;
+   else if (q7[i].value == "ps3") {
+		   ps = ps+3;
+
+  break;
+ }
+}
+
+var q8 = document.getElementsByName('q8');
+
+for (var i = 0, length = q8.length; i < length; i++)
+{
+ if (q8[i].checked)
+ {
+
+	if (q8[i].value == "an0") {
+		an = an+0;  
+	else if (q8[i].value == "an1") {
+	   an = an+1;  
+   else if (q8[i].value == "an2") {
+	   an = an+2;
+   else if (q8[i].value == "an3") {
+	   an = an+3;
+
+  break;
+ }
+}
+
+var q9 = document.getElementsByName('q9');
+
+for (var i = 0, length = q9.length; i < length; i++)
+{
+ if (q9[i].checked)
+ {
+
+	if (q9[i].value == "an0") {
+		an = an+0;  
+	else if (q9[i].value == "an1") {
+	   an = an+1;  
+   else if (q9[i].value == "an2") {
+	   an = an+2;
+   else if (q9[i].value == "an3") {
+	   an = an+3;
+
+  break;
+ }
+}
+
+var q10 = document.getElementsByName('q10');
+
+for (var i = 0, length = q10.length; i < length; i++)
+{
+ if (q10[i].checked)
+ {
+
+	if (q10[i].value == "an0") {
+		an = an+0;  
+	else if (q10[i].value == "an1") {
+	   an = an+1;  
+   else if (q10[i].value == "an2") {
+	   an = an+2;
+   else if (q10[i].value == "an3") {
+	   an = an+3;
+
+  break;
+ }
+}
+
+var q11 = document.getElementsByName('q11');
+
+for (var i = 0, length = q11.length; i < length; i++)
+{
+ if (q11[i].checked)
+ {
+
+	if (q11[i].value == "an0") {
+		an = an+0;  
+	else if (q11[i].value == "an1") {
+	   an = an+1;  
+   else if (q11[i].value == "an2") {
+	   an = an+2;
+   else if (q11[i].value == "an3") {
+	   an = an+3;
+
+  break;
+ }
+}
+
+var q12 = document.getElementsByName('q12');
+
+for (var i = 0, length = q12.length; i < length; i++)
+{
+ if (q12[i].checked)
+ {
+
+	if (q12[i].value == "ps0") {
+		ps = ps+0;  
+	else if (q12[i].value == "ps1") {
+	   ps = ps+1;  
+   else if (q12[i].value == "ps2") {
+	   ps = ps+2;
+   else if (q12[i].value == "ps3") {
+		   ps = ps+3;
+
+  break;
+ }
+}
+
+var q13 = document.getElementsByName('q13');
+
+for (var i = 0, length = q13.length; i < length; i++)
+{
+ if (q13[i].checked)
+ {
+
+	if (q13[i].value == "df0") {
+		df = df+0;  
+	else if (q13[i].value == "df1") {
+	   df = df+1;  
+   else if (q13[i].value == "df2") {
+	   df = df+2;
+   else if (q13[i].value == "df3") {
+		   df = df+3;
+
+  break;
+ }
+}
+
+var q14 = document.getElementsByName('q14');
+
+for (var i = 0, length = q14.length; i < length; i++)
+{
+ if (q14[i].checked)
+ {
+
+	if (q14[i].value == "df0") {
+		df = df+0;  
+	else if (q14[i].value == "df1") {
+	   df = df+1;  
+   else if (q14[i].value == "df2") {
+	   df = df+2;
+   else if (q14[i].value == "df3") {
+		   df = df+3;
+
+  break;
+ }
+}
+
+var q15 = document.getElementsByName('q15');
+
+for (var i = 0, length = q15.length; i < length; i++)
+{
+ if (q15[i].checked)
+ {
+
+	if (q15[i].value == "an0") {
+		an = an+0;  
+	else if (q15[i].value == "an1") {
+	   an = an+1;  
+   else if (q15[i].value == "an2") {
+	   an = an+2;
+   else if (q15[i].value == "an3") {
+	   an = an+3;
+
+  break;
+ }
+}
+
+var q16 = document.getElementsByName('q16');
+
+for (var i = 0, length = q16.length; i < length; i++)
+{
+ if (q16[i].checked)
+ {
+
+	if (q16[i].value == "df0") {
+		df = df+0;  
+	else if (q16[i].value == "df1") {
+	   df = df+1;  
+   else if (q16[i].value == "df2") {
+	   df = df+2;
+   else if (q16[i].value == "df3") {
+		   df = df+3;
+
+  break;
+ }
+}
+
+var q17 = document.getElementsByName('q17');
+
+for (var i = 0, length = q17.length; i < length; i++)
+{
+ if (q17[i].checked)
+ {
+
+	if (q17[i].value == "at0") {
+		at = at+0;  
+	else if (q17[i].value == "at1") {
+	   at = at+1;  
+   else if (q17[i].value == "at2") {
+	   at = at+2;
+   else if (q17[i].value == "at3") {
+		   at = at+3;
+
+  break;
+ }
+}
+
+var q18 = document.getElementsByName('q18');
+
+for (var i = 0, length = q18.length; i < length; i++)
+{
+ if (q18[i].checked)
+ {
+
+	if (q18[i].value == "df0") {
+		df = df+0;  
+	else if (q18[i].value == "df1") {
+	   df = df+1;  
+   else if (q18[i].value == "df2") {
+	   df = df+2;
+   else if (q18[i].value == "df3") {
+		   df = df+3;
+
+  break;
+ }
+}
+
+var q19 = document.getElementsByName('q19');
+
+for (var i = 0, length = q19.length; i < length; i++)
+{
+ if (q19[i].checked)
+ {
+
+	if (q19[i].value == "at0") {
+		at = at+0;  
+	else if (q19[i].value == "at1") {
+	   at = at+1;  
+   else if (q19[i].value == "at2") {
+	   at = at+2;
+   else if (q19[i].value == "at3") {
+		   at = at+3;
+
+  break;
+ }
+}
+
+var q20 = document.getElementsByName('q20');
+
+for (var i = 0, length = q20.length; i < length; i++)
+{
+ if (q20[i].checked)
+ {
+
+	if (q20[i].value == "at0") {
+		at = at+0;  
+	else if (q20[i].value == "at1") {
+	   at = at+1;  
+   else if (q20[i].value == "at2") {
+	   at = at+2;
+   else if (q20[i].value == "at3") {
+		   at = at+3;
+
+  break;
+ }
+}
+
+var q21 = document.getElementsByName('q21');
+
+for (var i = 0, length = q21.length; i < length; i++)
+{
+ if (q21[i].checked)
+ {
+
+	if (q21[i].value == "ps0") {
+		ps = ps+0;  
+	else if (q21[i].value == "ps1") {
+	   ps = ps+1;  
+   else if (q12[i].value == "ps2") {
+	   ps = ps+2;
+   else if (q21[i].value == "ps3") {
+		   ps = ps+3;
+
+  break;
+ }
+}
+
+var q22 = document.getElementsByName('q22');
+
+for (var i = 0, length = q22.length; i < length; i++)
+{
+ if (q22[i].checked)
+ {
+
+	if (q22[i].value == "at0") {
+		at = at+0;  
+	else if (q22[i].value == "at1") {
+	   at = at+1;  
+   else if (q22[i].value == "at2") {
+	   at = at+2;
+   else if (q22[i].value == "at3") {
+		   at = at+3;
+
+  break;
+ }
+}
+
+var q23 = document.getElementsByName('q23');
+
+for (var i = 0, length = q23.length; i < length; i++)
+{
+ if (q23[i].checked)
+ {
+
+	if (q23[i].value == "ps0") {
+		ps = ps+0;  
+	else if (q23[i].value == "ps1") {
+	   ps = ps+1;  
+   else if (q23[i].value == "ps2") {
+	   ps = ps+2;
+   else if (q23[i].value == "ps3") {
+		   ps = ps+3;
+
+  break;
+ }
+}
+
+var q24 = document.getElementsByName('q24');
+
+for (var i = 0, length = q24.length; i < length; i++)
+{
+ if (q24[i].checked)
+ {
+
+	if (q24[i].value == "ps0") {
+		ps = ps+0;  
+	else if (q24[i].value == "ps1") {
+	   ps = ps+1;  
+   else if (q24[i].value == "ps2") {
+	   ps = ps+2;
+   else if (q24[i].value == "ps3") {
+		   ps = ps+3;
+
+  break;
+ }
+}
+
+var q25 = document.getElementsByName('q25');
+
+for (var i = 0, length = q25.length; i < length; i++)
+{
+ if (q25[i].checked)
+ {
+
+	if (q25[i].value == "at0") {
+		at = at+0;  
+	else if (q25[i].value == "at1") {
+	   at = at+1;  
+   else if (q25[i].value == "at2") {
+	   at = at+2;
+   else if (q25[i].value == "at3") {
+		   at = at+3;
+
+  break;
+ }
+}
+
+var q6 = document.getElementsByName('q6');
+
+for (var i = 0, length = q6.length; i < length; i++)
+{
+ if (q6[i].checked)
+ {
+
+ 	if (q6[i].value == "true") {
+ 		p = p+2.5; 
+ 		document.getElementById("msg6").innerHTML = "Genial!"} 
+ 		else {document.getElementById("msg6").innerHTML =  "<img src= respostaex2.png> "};
+
+  break;
+ }
+}
+
+var q7 = document.getElementsByName('q7');
+
+for (var i = 0, length = q7.length; i < length; i++)
+{
+ if (q7[i].checked)
+ {
+
+ 	if (q7[i].value == "true") {
+ 		p = p+2.5; 
+ 		document.getElementById("msg7").innerHTML = "Genial!"} 
+ 		else {document.getElementById("msg7").innerHTML = " <img src= respostaex3a.png>"};
+
+  break;
+ }
+}
+
+var q8 = document.getElementsByName('q8');
+
+for (var i = 0, length = q8.length; i < length; i++)
+{
+ if (q8[i].checked)
+ {
+
+ 	if (q8[i].value == "true") {
+ 		p = p+2.5; 
+ 		document.getElementById("msg8").innerHTML = "Genial!"} 
+ 		else {document.getElementById("msg8").innerHTML = "<img src= respostaex4a.png>"};
+
+  break;
+ }
+}
+
+var q9 = document.getElementsByName('q9');
+
+for (var i = 0, length = q9.length; i < length; i++)
+{
+ if (q9[i].checked)
+ {
+
+ 	if (q9[i].value == "true") {
+ 		p = p+2.5;
+ 		document.getElementById("msg9").innerHTML = "Genial!"} 
+ 		else {document.getElementById("msg9").innerHTML = "<img src= respostaex3b.png>"};
+
+  break;
+ }
+}
+
+var q10 = document.getElementsByName('q10');
+
+for (var i = 0, length = q10.length; i < length; i++)
+{
+ if (q10[i].checked)
+ {
+
+ 	if (q10[i].value == "true") {
+ 		p = p+2.5; 
+ 		document.getElementById("msg10").innerHTML = "Genial!"} 
+ 		else {document.getElementById("msg10").innerHTML = "<img src= respostaex4b.png>"};
+
+  break;
+ }
+}
+console.log(p)
+console.log(an)
+console.log(df)
+console.log(at)
+console.log(di)
+console.log(ps)
+var txt = document.getElementById('txt')
+
+if (p == 15) {
+	txt.innerHTML = "Enhorabona! Has obtingut 15/15!"
+} else if (p < 15 && p > 10) {
+	txt.innerHTML = "Ben fet. Has obtingut " + p + "/15!"
+} else if (p <= 10 && p > 5) {
+	txt.innerHTML = "Pots millorar. Has obtingut " + p + "/15!"
+} else if (p <= 5) {
+  txt.innerHTML =	"Has d'estudiar molt! Has obtingut" + p + "/15!"
+}
+
+}
